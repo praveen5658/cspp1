c5eca3c7151bcb12abacea083d5e990b26416d17
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-08-01 20:35:16 +0530

    compare_AB

---
+++ cspp1_practise/m3/compare_AB.py
@@ -0,0 +1,10 @@
+varA="hello"
+varB="world"
+if(type(varA)==str or type(varB)==str):
+	print("string involved")
+elif(varA>varB):
+	print("bigger")
+elif(varA==varB):
+	print("equal")
+else:
+		print("smaller")
