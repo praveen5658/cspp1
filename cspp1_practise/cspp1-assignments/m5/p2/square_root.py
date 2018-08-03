03b0ddc39267248e6d71887905dbcca95bcc337c
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-08-03 17:36:27 +0530

    p2 folder

---
+++ cspp1_practise/cspp1-assignments/m5/p2/square_root.py
@@ -0,0 +1,22 @@
+'''
+Author : Praveen
+Date : 03-08-2018
+
+'''
+def main():
+    ''' This section tells square root of given number in Approximation method'''
+    s_t = int(input())
+    epsi_lon = 0.01
+    gu_ess = 0.0
+    incre_ment = 0.01
+    while abs(gu_ess**2 - s_t) >= epsi_lon:
+        if gu_ess <= s_t:
+            gu_ess += incre_ment
+        else:
+            break
+    if abs(gu_ess**2 - s_t) >= epsi_lon:
+        print(" Failed to compute Square root of given number")
+    else:
+        print(gu_ess)
+if __name__ == "__main__":
+    main()
