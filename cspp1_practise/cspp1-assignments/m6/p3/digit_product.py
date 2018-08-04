45ea9ab10c9efebf2fceb23e2843735794758508
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-08-04 10:35:24 +0530

    " -> 4 of 7 passed. pylint: 10.00/10 "

---
+++ cspp1_practise/cspp1-assignments/m6/p3/digit_product.py
@@ -0,0 +1,18 @@
+'''
+Author :Praveen
+Date : 04-08-2018
+'''
+def main():
+    '''
+    THis function prints the product of digits in the given number
+    '''
+    int_input = int(input())
+    rem_inder = 0
+    pro_duct = 1
+    while int_input != 0:
+        rem_inder = int_input % 10
+        pro_duct = pro_duct * rem_inder
+        int_input = int_input // 10
+    print(pro_duct)
+if __name__ == "__main__":
+    main()
