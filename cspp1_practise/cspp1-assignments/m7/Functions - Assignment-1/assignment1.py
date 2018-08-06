832ca6ad812829105fd3c1ba44f32b528c8df8b3
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-08-06 21:50:50 +0530

    " -> 0 of 10 passed. pylint: 0/0 "

--- cspp1_practise/cspp1-assignments/m7/Functions - Assignment-1/assignment1.py
+++ cspp1_practise/cspp1-assignments/m7/Functions - Assignment-1/assignment1.py
@@ -7,13 +7,13 @@ def paying_debt_off_inayear(balance_unpaid, annual_interest_rate, monthly_paymen
     '''This function calculates the remaining balance '''
     balance_copy = balance_unpaid
 	iterator_i = 1
-    while iterator_i < 12:
+    while iterator_i <= 12:
         montly_interest_rate = annual_interest_rate / 12.0
         minimum_monthly_payment = monthly_payment_rate*balance_copy
         monthly_unpaid_balance = balance_copy - minimum_monthly_payment
         balance_copy = monthly_unpaid_balance + (montly_interest_rate * monthly_unpaid_balance)
         iterator_i += 1
-    return "Remaining balance: "+str(round(balance_copy, 2)) 
+    return "Remaining balance: "+str(round(balance_copy, 2))
 def main():
     '''This is main function'''
     data = input()
@@ -21,5 +21,5 @@ def main():
     data = list(map(float, data))
     print(paying_debt_off_inayear(data[0], data[1], data[2]))
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
