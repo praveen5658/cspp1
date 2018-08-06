9c8e03a5d795a61b3e77a6bce9fff2e5dea700c7
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-08-06 22:06:11 +0530

    " -> 10 of 10 passed. pylint: 9.66/10 "

--- cspp1_practise/cspp1-assignments/m7/Functions - Assignment-3/assignment3.py
+++ cspp1_practise/cspp1-assignments/m7/Functions - Assignment-3/assignment3.py
@@ -2,31 +2,40 @@
 Author : Praveen
 Date : 06-08-2018
 '''
-def paying_debt_off_in_a_year(balance, annual):
-    '''This function Calculate minimum payment '''
-    lower = balance / 12.0
-    upper = balance * (1 + annual / 12.0) ** 12 / 12
-    fixed = (lower + upper) / 2.0
-    i = 1
-    while True:
-        remain = balance
-        for i in range(1, 13):
-            remain = (remain - fixed) * (1 + annual / 12.0)
-        if remain > 0:
-            lower = fixed
-        elif remain <= 0 and remain >= -0.0001:
-            break
+def paying_debt(balance_unpaid, annual_interest_rate, guess_num):
+    '''This function in paying debt'''
+    balance_copy = balance_unpaid
+
+    iterator_i = 1
+    while iterator_i <= 12:
+        montly_interest_rate = annual_interest_rate / 12
+        monthly_unpaid_balance = balance_copy - guess_num
+        balance_copy = monthly_unpaid_balance + (montly_interest_rate * monthly_unpaid_balance)
+        iterator_i += 1
+    return balance_copy
+
+def paying_debt_off_inayear(balance_unpaid, annual_interest_rate):
+    '''This function calculates paying deft off in a year'''
+    approximation_val = 0.03
+    montly_interest_rate = annual_interest_rate /12.0
+    high_val = (balance_unpaid * (1 + montly_interest_rate) ** 12) / 12.0
+    low_val = balance_unpaid / 12
+    middle_val = (low_val + high_val) / 2.0
+
+    while abs(paying_debt(balance_unpaid, annual_interest_rate, middle_val)) >= approximation_val:
+        if approximation_val <= paying_debt(balance_unpaid, annual_interest_rate, middle_val):
+            low_val = middle_val
         else:
-            upper = fixed
-        fixed = (lower + upper) / 2.0
-    ans = "Lowest Payment: " + str(round(fixed, 2))
-    print(ans)
+            high_val = middle_val
+        middle_val = (low_val + high_val) / 2.0
+    min_pay = middle_val
+    return "Lowest Payment: "+str(round(min_pay, 2))
+
 def main():
-    ''' Main Function'''
+    '''This is main function'''
     data = input()
-    # data = "4773 0.2"
     data = data.split(' ')
     data = list(map(float, data))
-    paying_debt_off_in_a_year(data[0], data[1])
+    print(paying_debt_off_inayear(data[0], data[1]))
 if __name__ == "__main__":
     main()
