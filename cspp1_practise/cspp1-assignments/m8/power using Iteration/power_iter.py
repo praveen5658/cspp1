189b0c4cc0dad73b0afd242c5d6cf9581db17e03
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-08-07 14:29:13 +0530

    power_iter

---
+++ cspp1_practise/cspp1-assignments/m8/power using Iteration/power_iter.py
@@ -0,0 +1,21 @@
+'''
+Author : Praveen
+Date : 07-08-2018
+'''
+def iter_power(base, exp):
+    '''
+    FUnction for computing Power
+    '''
+    res_lt = 1
+    for loop_vaiable in range(exp):
+        res_lt *= base
+        loop_vaiable += 1
+    return res_lt
+def main():
+    '''This is main function'''
+    data = input()
+    data = data.split()
+    print(iter_power(float(data[0]), int(data[1])))
+
+if __name__ == "__main__":
+    main()
