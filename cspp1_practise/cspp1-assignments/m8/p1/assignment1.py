1721a0214ddcec40b3d059e35de5c37b665587c1
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-08-07 20:02:50 +0530

    " -> 10 of 10 passed. pylint: 10.00/10 "

--- cspp1_practise/cspp1-assignments/m8/p1/assignment1.py
+++ cspp1_practise/cspp1-assignments/m8/p1/assignment1.py
@@ -2,15 +2,16 @@
 Author : Praveen
 Date : 07-08-2018
 '''
-def factorial(n):
+def factorial(input_n):
     '''
     This function computes factorial of number using recursion
     '''
-    if n == 1 or n == 0:
+    if input_n in (0, 1):
         return 1
-    return n * factorial(n-1)
+    return input_n * factorial(input_n-1)
 def main():
-    a = input()
-    print(factorial(int(a)))
+    '''This is main function'''
+    input_a = input()
+    print(factorial(int(input_a)))
 if __name__ == "__main__":
     main()
