696f699be641e0ad4935585fb40a8c0a5052279a
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-08-08 14:42:33 +0530

    odd_tuples

---
+++ cspp1_practise/cspp1-assignments/m9/Odd Tuples Exercise/odd_tuples.py
@@ -0,0 +1,20 @@
+'''
+Author : Praveen
+Date : 07-08-2018
+'''
+def odd_tuples(input_tup):
+    '''
+    Function will print odd index values in tuple
+    '''
+    return input_tup[::2]
+def main():
+    '''This is main function'''
+    data = input()
+    data = data.split()
+    a_tup = ()
+    le_n = len(data)
+    for j in range(le_n):
+        a_tup += (int(data[j]),)
+    print(odd_tuples(a_tup))
+if __name__ == "__main__":
+    main()
