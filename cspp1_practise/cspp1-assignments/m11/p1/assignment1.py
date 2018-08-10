5b83d77689d32dc631d1b920cefba430051de947
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-08-10 14:59:42 +0530

    " -> 9 of 9 passed. pylint: 8.00/10 "

--- cspp1_practise/cspp1-assignments/m11/p1/assignment1.py
+++ cspp1_practise/cspp1-assignments/m11/p1/assignment1.py
@@ -3,33 +3,29 @@ Author : Praveen
 Date : 10-08-2018
 '''
 
-def get_word_score(word, n):
+def get_word_score(word, num_ber):
     """
-    Returns the score for a word. Assumes the word is a valid word.
+    Returns the score for a word. Assu_mes the word is a valid word.
 
-    The score for a word is the sum of the points for letters in the
+    The score for a word is the su_m of the points for letters in the
     word, multiplied by the length of the word, PLUS 50 points if all n
     letters are used on the first turn.
 
     Letters are scored as in Scrabble; A is worth 1, B is worth 3, C is
-    worth 3, D is worth 2, E is worth 1, and so on (see SCRABBLE_LETTER_VALUES)
+    worth 3, D is worth 2, E is worth 1, and so on (see scrabble_letters)
 
     word: string (lowercase letters)
     n: integer (HAND_SIZE; i.e., hand size required for additional points)
     returns: int >= 0
     """
-    SCRABBLE_LETTER_VALUES = {
-    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
-    'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
-    's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
-    }
-    sum = 0
+    scrabble_letters = {'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,'s': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
+    su_m = 0
     le_n = len(word)
     for lo_op in word:
-        sum += SCRABBLE_LETTER_VALUES[lo_op]
-    if n == le_n:
-        return (sum * n)+50
-    return sum * le_n
+        su_m += scrabble_letters[lo_op]
+    if num_ber == le_n:
+        return (su_m * num_ber)+50
+    return su_m * le_n
 
 
 def main():
