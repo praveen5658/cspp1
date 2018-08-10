86a48f24380cc8c72739c99c8a685de1f5354338
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-08-10 17:27:12 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- cspp1_practise/cspp1-assignments/m11/p3/assignment3.py
+++ cspp1_practise/cspp1-assignments/m11/p3/assignment3.py
@@ -3,26 +3,27 @@ Author : Praveen
 Date : 10-08-2018
 '''
 
-def isValidWord(word, hand, wordList):
+def isvalidword(word, hand, wordlist):
     """
-    Returns True if word is in the wordList and is entirely
+    Returns True if word is in the wordlist and is entirely
     composed of letters in the hand. Otherwise, returns False.
 
-    Does not mutate hand or wordList.
+    Does not mutate hand or wordlist.
     word: string
     hand: dictionary (string -> int)
-    wordList: list of lowercase strings
+    wordlist: list of lowercase strings
     """
     count = 0
     le_n = len(word)
     for lo_op in word:
         if lo_op in hand:
             count += 1
-    return bool(count == le_n and word in wordList)
+    return bool(count == le_n and word in wordlist)
 
 
 
 def main():
+    '''This is main function'''
     word = input()
     nu_m = int(input())
     adict = {}
@@ -32,7 +33,7 @@ def main():
         adict[spl_it[0]] = int(spl_it[1])
         ite_r += 1
     l_2 = input().split()
-    print(isValidWord(word, adict, l_2))
+    print(isvalidword(word, adict, l_2))
 
 
 if __name__ == "__main__":
