aa2f4cbea8bd11549619531d94327996dd46f7c8
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-08-14 11:17:00 +0530

    " -> 0 of 3 passed. pylint: 8.11/10 "

--- cspp1_practise/cspp1-assignments/m14/CodeCampPoker/poker.py
+++ cspp1_practise/cspp1-assignments/m14/CodeCampPoker/poker.py
@@ -16,12 +16,19 @@ def is_straight(hand):
     le_n = len(hand)
     forward_sequence = 'A123456789TJQKA'
     backward_sequence = 'AKQJT987654321A'
+    dictionary = {'2':0, '3':1, '4':2, '5':3, '6':4, '7':5, '8':6, '9':7, 'T':8, 'J':9, 'Q':10, 'K':11, 'A':12}
     st_r = ""
+    st_r1 = ""
     for lo_op in range(le_n):
         st_r += hand[lo_op][0]
-    for lo_op in range(11):
+        st_r1 += dictionary[hand[lo_op][0]]
+    l = list(int(st_r1)).sort()
+    '''for lo_op in range(11):
         if forward_sequence[lo_op:lo_op + 5] == st_r or backward_sequence[lo_op:lo_op + 5]:
-            return 1
+            return 1'''
+    for lo_op in l:
+    	if lo_op != lo_op - 1:
+    		return 1
     return 0
 
 def is_flush(hand):
