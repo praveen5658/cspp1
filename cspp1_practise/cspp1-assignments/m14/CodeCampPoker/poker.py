a0403600321904604f3e4536798c43300f39f8f5
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-08-14 11:50:36 +0530

    " -> 3 of 3 passed. pylint: 7.69/10 "

--- cspp1_practise/cspp1-assignments/m14/CodeCampPoker/poker.py
+++ cspp1_practise/cspp1-assignments/m14/CodeCampPoker/poker.py
@@ -30,7 +30,7 @@ def is_straight(hand):
     	for lo_op1 in range(le_n):
     		if hand[lo_op1][0] == lo_op:
     			st_r += lo_op
-    print(st_r)
+    # print(st_r)
     for lo_op in range(11):
         if forward_sequence[lo_op:lo_op + 5] == st_r:
             return 1
