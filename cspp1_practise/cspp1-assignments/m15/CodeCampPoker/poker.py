f434dd5fe5f1007e9322fdfb8fe18986fa0e27e9
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-08-16 12:45:29 +0530

    " -> 7 of 19 passed. pylint: 9.71/10 "

--- cspp1_practise/cspp1-assignments/m15/CodeCampPoker/poker.py
+++ cspp1_practise/cspp1-assignments/m15/CodeCampPoker/poker.py
@@ -97,7 +97,7 @@ def max_face_count(hand):
     '''This will check the condition full house'''
     dictionary = {'A':0}
     l_1 = list("KQJA")
-    print(l_1)
+    print(hand)
     for lo_op in hand:
         if lo_op[0] in ('A', 'K', 'Q', 'J'):
             dictionary[lo_op[0]] += 1
