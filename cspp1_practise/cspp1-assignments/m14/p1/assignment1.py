eb9cfe0eb5715da932e1a82f0d3f53793418c197
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-08-21 22:57:22 +0530

    " -> 4 of 5 passed. pylint: 8.97/10 "

--- cspp1_practise/cspp1-assignments/m14/p1/assignment1.py
+++ cspp1_practise/cspp1-assignments/m14/p1/assignment1.py
@@ -105,12 +105,12 @@ class Message(object):
         The dictionary maps every uppercase and lowercase letter to a
         character shifted down the alphabet by the input shift. The dictionary
         should have 52 keys of all the uppercase letters and all the lowercase
-        letters only.        
+        letters only.
         shift (integer): the amount by which to shift every letter of the
         alphabet. 0 <= shift < 26
 
-        Returns: a dictionary mapping a letter (string) to 
-                 another letter (string). 
+        Returns: a dictionary mapping a letter (string) to
+                 another letter (string).
         '''
         cipher_dictionary = {}
         upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
