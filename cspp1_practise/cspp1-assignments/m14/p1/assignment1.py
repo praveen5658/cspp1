4f20e44b42373c62202d00615610f30934892fd3
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-08-21 22:56:45 +0530

    " -> 4 of 5 passed. pylint: 8.21/10 "

--- cspp1_practise/cspp1-assignments/m14/p1/assignment1.py
+++ cspp1_practise/cspp1-assignments/m14/p1/assignment1.py
@@ -88,7 +88,6 @@ class Message(object):
     def get_message_text(self):
         '''
         Used to safely access self.message_text outside of the class
-        
         Returns: self.message_text
         '''
         return self.message_text
@@ -97,19 +96,16 @@ class Message(object):
     def get_valid_words(self):
         '''
         Used to safely access a copy of self.valid_words outside of the class
-        
         Returns: a COPY of self.valid_words
         '''
         return self.valid_words[:]
-        
     def build_shift_dict(self, shift):
         '''
-        Creates a dictionary that can be used to apply a cipher to a letter.
+        Creates a dictionary that can be used to apply a cipher to a letter
         The dictionary maps every uppercase and lowercase letter to a
         character shifted down the alphabet by the input shift. The dictionary
         should have 52 keys of all the uppercase letters and all the lowercase
         letters only.        
-        
         shift (integer): the amount by which to shift every letter of the
         alphabet. 0 <= shift < 26
 
@@ -130,7 +126,6 @@ class Message(object):
         Applies the Caesar Cipher to self.message_text with the input shift
         Creates a new string that is self.message_text shifted down the
         alphabet by some number of characters determined by the input shift
-        
         shift (integer): the shift with which to encrypt the message.
         0 <= shift < 26
 
