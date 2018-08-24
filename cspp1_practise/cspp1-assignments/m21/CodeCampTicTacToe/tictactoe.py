c8e49cc2ea7dc43f366a104c2109f5338e7be817
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-08-24 10:17:12 +0530

    " -> 14 of 14 passed. pylint: 8.97/10 "

--- cspp1_practise/cspp1-assignments/m21/CodeCampTicTacToe/tictactoe.py
+++ cspp1_practise/cspp1-assignments/m21/CodeCampTicTacToe/tictactoe.py
@@ -36,60 +36,7 @@ def is_vertical(main_list, turn):
             return True
         cou = 0
     return False
-def winner(row_one, row_two, row_three, turn):
-    game_printing(row_one, row_two, row_three)
-    print()
-    print("Congratulations user:", turn, " You Won!")
-def get_input(turn):
-    print()
-    print("Turn :",turn)
-    print(" Enter the position")
-    li_st = input().split()
-    return int(li_st[0]), int(li_st[1])
-def game_printing(row_one, row_two, row_three):
-    print("Game is :")
-    print()
-    print(' '.join(row_one))
-    print(' '.join(row_two))
-    print(' '.join(row_three))
-    # boolean = is_horizontal(row_one, row_two, row_three)
-    # print(boolean)
-    # if boolean:
-    #     print("Game Over !")
-    #     print("Congratulations ",turn," You won!")
-    #     return 4, 4
-    # else:
-        
 def main():
-    # cou = 0
-    # num = 0
-    # row_one = ['-','-','-']
-    # row_two = ['-','-','-']
-    # row_three = ['-','-','-']
-    # main_list = [row_one, row_two, row_three]
-    # while cou < 9:
-    #     if cou % 2 == 0:
-    #         turn = 'X'
-    #     else:
-    #         turn = 'O' 
-    #     game_printing(row_one, row_two, row_three)
-    #     (row, column) = get_input(turn)
-    #     main_list[row][column] = turn
-    #     boolean = (is_horizontal(main_list, turn) or is_vertical(main_list, turn) or is_diagnol_forward(main_list, turn) or is_diagnol_backward(main_list, turn))
-    #     if boolean:
-    #         winner(row_one, row_two, row_three, turn)
-    #         num = 1
-    #         break
-    #     cou += 1
-        # game_printing(row_one, row_two, row_three)
-        # (row, column) = get_input('O')
-        # main_list[row][column] = 'O'
-        # print(is_horizontal(main_list, 'O'))
-        # print(is_vertical(main_list, 'O'))
-        # print(is_diagnol_forward(main_list, 'O'))
-        # print(is_diagnol_backward(main_list, 'O'))
-    # if num == 0:
-    #     print("Game is Tie")
     cou = 0
     x_count = 0
     o_count = 0
@@ -115,7 +62,10 @@ def main():
         print("invalid game")
     else:
         turn_x = 'x'
-        boolean_x = (is_horizontal(main_list, turn_x) or is_vertical(main_list, turn_x) or is_diagnol_forward(main_list, turn_x) or is_diagnol_backward(main_list, turn_x))
+        boolean_x = (is_horizontal(main_list, turn_x)
+                     or is_vertical(main_list, turn_x)
+                     or is_diagnol_forward(main_list, turn_x)
+                     or is_diagnol_backward(main_list, turn_x))
         turn_o = 'o'
         boolean_o = (is_horizontal(main_list, turn_o) or is_vertical(main_list, turn_o) or is_diagnol_forward(main_list, turn_o) or is_diagnol_backward(main_list, turn_o))
         if boolean_x and boolean_o:
