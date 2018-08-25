f4672035168725de64f5fb32000421e06dad18b5
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-08-25 11:11:43 +0530

    " -> 0 of 3 passed. pylint: 3.00/10 "

---
+++ cspp1_practise/cspp1-assignments/m22/assignment4/print_dictionary.py
@@ -0,0 +1,20 @@
+'''
+Write a function to print a dictionary with the keys in sorted order along with the
+frequency of each word. Format of the printing should be one key per line and separate
+the key and frequency with a SPACE - SPACE.
+Author : Praveen
+Date: 25-08-2018
+'''
+
+def print_dictionary(dictionary):
+    '''Sub Function'''
+    li_st = list(dictionary.keys()).sorted()
+    sample_dictionary = {}
+    for lo_op in li_st:
+        print(lo_op, " - ", dictionary[lo_op])
+def main():
+    '''Main Function'''
+    dictionary = eval(input())
+    print_dictionary(dictionary)
+if __name__ == '__main__':
+    main()
