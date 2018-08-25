cda51a8b11aee965af3a19b22e9075d0f4372005
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-08-25 11:35:35 +0530

    " -> 4 of 26 passed. pylint: 8.00/10 "

--- cspp1_practise/cspp1-assignments/m22/Check Sudoku/check_sudoku.py
+++ cspp1_practise/cspp1-assignments/m22/Check Sudoku/check_sudoku.py
@@ -7,13 +7,28 @@
     Complete the check_sudoku function to check if the given grid
     satisfies all the sudoku rules given in the statement above.
 '''
-
+def is_line(li_st):
+    sample_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    sample_dictionary = {}
+    for lo_op in li_st:
+        if lo_op in sample_list:
+            if lo_op not in sample_dictionary:
+                sample_dictionary[lo_op] = 1
+            else:
+                return False
+    le_n = len(list(sample_dictionary.keys()))
+    if le_n == 9:
+        return True
+    return False
 def check_sudoku(sudoku):
     '''
         Your solution goes here. You may add other helper functions as needed.
         The function has to return True for a valid sudoku grid and false otherwise
     '''
-    pass
+    for lo_op in sudoku:
+        boolean = is_line(lo_op)
+        if boolean:
+            return True
 
 def main():
     '''
@@ -30,7 +45,6 @@ def main():
         row = input().split(' ')
         sudoku.append(row)
     # call solution function and print result to console
-    print(sudoku)
     print(check_sudoku(sudoku))
 
 if __name__ == '__main__':
